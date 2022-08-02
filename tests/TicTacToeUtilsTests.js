edd6d1ab748eb8c206ee93d67977f9914a72ee7d
Marcos27SG <marcos.saavedra@jalasoft.com>
2022-08-02 08:15:20 -0400

    New Test Branch Commit

--- tests/TicTacToeUtilsTests.js
+++ tests/TicTacToeUtilsTests.js
@@ -28,3 +28,18 @@ describe("the function", function () {
   });
 });
 
+describe("the function", function () {
+  it("should not work", function () {
+    var ticTacToeUtils = new tictactoeUtils.TicTacToeUtils();
+    var board = ticTacToeUtils.buildBoard(5);
+    console.log("G", board);
+    var expectedBoard = {
+        board: [  
+        ],
+        empty: [
+        ]
+        };
+    assert.notEqual(board, expectedBoard);
+  });
+});
+
